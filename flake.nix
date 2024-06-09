7d7982dd3bb14b39c0d09340a10a05d9f03d3300
Hammy <58985301+sgoudham@users.noreply.github.com>
2024-06-09 19:28:24 +0100

    build(nix): add nix shell (#4)

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./shell.nix { };
+      });
+    };
+}
